5525290c5ef2d2e603c051908c68997368544049
luojiayu <luojiayu@ghostcloud.cn>
2021-04-13 15:30:43 +0800

    初始化spring-cloud-parent

---
+++ spring-cloud-parent/src/main/java/cn/code/SpringCloudParentApplication.java
@@ -0,0 +1,13 @@
+package cn.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudParentApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-service-eureka/src/main/java/cn/code/SpringCloudServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceEurekaApplication.class, args);
+    }
+
+}
